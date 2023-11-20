28d9c61208783821683350d3ea3037ca21264c77
EasyFreezzyy <vmezenin443@gmail.com>
2023-11-20 23:18:42 +0300

    Добавлено Хорошие пожелания и побуждение к правильному действию. )

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет мир");
+Console.WriteLine("Здравствуйте");
+Console.WriteLine("Желаю Вам хорошего дня!");
+Console.WriteLine("Хочу 4, поставьте, пожалуйста :3");
+    
+
