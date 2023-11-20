87436ef2eab9a6c64c3771c0380622c8af353875
EasyFreezzyy <vmezenin443@gmail.com>
2023-11-20 23:26:55 +0300

    Добавлено пожелание хорошего настроения и настойчивое упрашивание 4

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Здравствуйте");
-Console.WriteLine("Желаю Вам хорошего дня!");
+Console.WriteLine("Желаю Вам хорошего дня и великолепного настроения!");
 Console.WriteLine("Хочу 4, поставьте, пожалуйста :3");
+Console.WriteLine("Ну позязя...");
     
 
